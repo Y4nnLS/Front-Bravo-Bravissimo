6c9a6197eb3e9c1282d470620522b26b6ee33205
William Felipe <will25felipe@gmail.com>
2024-11-04 03:20:51 -0300

    feat: home page partialy done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          300: '#f4f4f488',
+          400: '#f1f1f1',
+          500: '#f4f4f4'
+        }
+      },
+    },
   },
   plugins: [],
 }
